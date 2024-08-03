a997d76ecced6b4092ffbf32cda8fe3ef9c9dc15
Kiran Biju <128731044+KiranBiju19@users.noreply.github.com>
2024-08-03 12:44:15 +0530

    Add files via upload

---
+++ bird.py
@@ -0,0 +1,51 @@
+
+from fastbook import *
+     
+
+urls = search_images_ddg('bird photos', max_images=1)
+len(urls),urls[0]
+
+dest = Path('bird.jpg')
+if not dest.exists():download_url(urls[0], dest, show_progress=False)
+     
+
+im = Image.open(dest)
+im.to_thumb(256, 256)
+
+
+searches = 'forest', 'bird'
+path = Path('bird_or_not')
+
+if not path.exists():
+  parent = (path)
+  parent.mkdir(exist_ok=True)
+  for o in searches:
+    dest = (path/o)
+    dest.mkdir(exist_ok=True)
+    results = search_images_ddg(f'{o} photo')
+    download_images(dest, urls=results[:200])
+    resize_images(dest, max_size=400, dest=dest)
+     
+
+failed = verify_images(get_image_files(path))
+failed.map(Path.unlink);
+
+     
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),
+    get_items=get_image_files,
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+    get_y=parent_label,
+    item_tfms=[Resize(192, method='squish')]
+).dataloaders(path)
+dls.show_batch(max_n=6)
+
+
+learn = cnn_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(3)
+     
+
+is_bird,_,probs = learn.predict(PILImage.create('bird.jpg'))
+print(f"This is a : {is_bird}.")
+print(f"Probablitiy it's a bird: {probs[0]:4f}")
